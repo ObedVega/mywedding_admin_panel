337aa49661d72c40c95daf8f636a5b04cf0186a2
obed vega <vega.obed@gmail.com>
2024-03-20 13:24:07 -0700

    test

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <form action="/action_page.php">
+    <div class="container">
+      <h1>Bienvenido</h1>
+      <p>Inicie sesión para administrar su cuenta.</p>
+      <hr>
+
+      <label for="email"><b>Correo electrónico</b></label>
+      <input type="text" placeholder="Ingresar correo electrónico" name="email" id="email" required>
+
+      <label for="psw"><b>Contraseña</b></label>
+      <input type="password" placeholder="Introduce tu contraseña" name="psw" id="psw" required>
+
+      <hr>
+      <p>Olvidaste tu <a href="#"> Contraseña</a>?</p>
+
+      <button type="submit" class="registerbtn">Inicio sesión</button>
+    </div>
+    
+    <div class="container signin">
+      <p>¿Todavia no tienes cuenta? <a href="/">Registrate aqui.</a>.</p>
+    </div>
+  </form>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,87 @@
+<template>
+  <form action="/action_page.php">
+  <div class="container">
+    <h1>Registro</h1>
+    <p>Por favor complete este formulario para crear una cuenta.</p>
+    <hr>
+
+    <label for="email"><b>Correo electrónico</b></label>
+    <input type="text" placeholder="Ingresar correo electrónico" name="email" id="email" required>
+
+    <label for="psw"><b>Contraseña</b></label>
+    <input type="password" placeholder="Introduce tu contraseña" name="psw" id="psw" required>
+
+    <label for="psw-repeat"><b>Repetir Contraseña</b></label>
+    <input type="password" placeholder="Repetir Contraseña" name="psw-repeat" id="psw-repeat" required>
+    <hr>
+    <p>Al crear una cuenta aceptas nuestra <a href="#"> Términos y Privacidad</a>.</p>
+
+    <button type="submit" class="registerbtn">Registarse</button>
+  </div>
+  
+  <div class="container signin">
+    <p>¿Ya tienes una cuenta? <a href="/">Iniciar sesión.</a>.</p>
+  </div>
+</form>
+</template>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Overwrite default styles of hr */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for the submit button */
+.registerbtn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+/* Add a blue text color to links */
+a {
+  color: dodgerblue;
+}
+
+/* Set a grey background color and center the text of the "sign in" section */
+.signin {
+/*  background-color: #f1f1f1; */
+  text-align: center;
+}
+</style>
