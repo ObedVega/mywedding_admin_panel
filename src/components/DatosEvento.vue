60674d9feecbf31378166636a07901c8dcee11e3
Vega <obed.vega@teradata.com>
2024-03-27 14:22:47 -0700

    link

---
+++ src/components/DatosEvento.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w3-panel">
+    <div><h3><b>Datos del evento</b></h3></div>
+    <form class="w3-container w3-card-4">
+      <div class="w3-container">
+        <h2>Datos del evento</h2>
+      </div>
+      <div class="w3-row-padding">
+        <div class="w3-half">
+          <label>Fecha del evento</label>
+          <input class="w3-input w3-border" type="date">
+        </div>
+        <div class="w3-half">
+          <label>Ubicacion</label>
+          <input class="w3-input w3-border" type="text" placeholder="Two">
+        </div>
+        <div class="w3-row-padding">
+          <button class="w3-btn w3-blue">Guardar</button>
+        </div>
+      </div>
+      <br>
+    </form>   
+  </div>  
+</template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -72,6 +72,16 @@
         <p>Aun necesitamos los datos de tu novia para poder generar la invitacion</p>
       </div>
   </div>
+  <div class="w3-panel">
+  <template v-if="user.nombre !== '' && user.nombreNovia !== ''">
+    <a :href="'https://mywedding-cyks.onrender.com/w/' + user.nombre.trim() + '&' + user.nombreNovia.trim()" target="_blank">
+      {{ 'https://mywedding-cyks.onrender.com/w/' + user.nombre.trim() + '&' + user.nombreNovia.trim() }}
+    </a>
+  </template>
+  <template v-else>
+    <p>Aun no hay un link para tu sitio.</p>
+  </template>
+</div>
   
   <div class="w3-panel" v-if="user.nombreNovia !== ''">
       <div><h3><b>Datos de la novia</b></h3></div>
@@ -79,28 +89,8 @@
   </div>
   <DatosNovia :uEmail="userEmail" v-if="user.nombreNovia === ''" @datos-actualizados="refreshData"/>
 
-  <div class="w3-panel">
-    <div><h3><b>Datos del evento</b></h3></div>
-    <form class="w3-container w3-card-4">
-      <div class="w3-container">
-        <h2>Datos del evento</h2>
-      </div>
-      <div class="w3-row-padding">
-        <div class="w3-half">
-          <label>Fecha del evento</label>
-          <input class="w3-input w3-border" type="date">
-        </div>
-        <div class="w3-half">
-          <label>Ubicacion</label>
-          <input class="w3-input w3-border" type="text" placeholder="Two">
-        </div>
-        <div class="w3-row-padding">
-          <button class="w3-btn w3-blue">Guardar</button>
-        </div>
-      </div>
-      <br>
-    </form>   
-  </div>  
+  <DatosEvento :uEmail="userEmail"/>
+  
 <!--
   <div class="w3-container">
     <h5>Recent Comments</h5>
@@ -299,6 +289,7 @@ import { mapState } from 'vuex';
 import AuthService from '@/services/AuthService.js';
 import CompletaInfo from '@/components/CompletaInfo.vue';
 import DatosNovia from '@/components/DatosNovia.vue'
+import DatosEvento from '@/components/DatosEvento.vue'
 import LogoutModal from '@/components/LogoutModal.vue';
 
 export default {
@@ -334,7 +325,8 @@ export default {
   components: {
     LogoutModal,
     CompletaInfo,
-    DatosNovia
+    DatosNovia,
+    DatosEvento
   },
   methods: {
     refreshData() {
